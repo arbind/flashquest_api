d840c9150d8c982ea249dc2a8353d229a6494585
Arbind <arbind@carbonfive.com>
2013-06-23 19:31:01 -0700

    lookup current user from access token in header

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::ApplicationController < ApplicationController
   layout 'api_v1'
   protect_from_forgery with: :null_session
+
+  def ensure_access_token
+    return @current_user if @current_user
+
+    token = request.headers[:access_token]
+    device = Device.find_by_access_token token
+    raise Exception::Unauthorized if device.nil?
+    @current_user = device.person
+    raise Exception::Unauthorized if @current_user.nil?
+  end
+
 end
