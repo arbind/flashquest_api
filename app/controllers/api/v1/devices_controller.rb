347cd478c9a0c5d693485457c88db5f66963c6e0
Arbind <arbind@carbonfive.com>
2013-06-23 19:37:52 -0700

    refactor

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -2,10 +2,7 @@ class Api::V1::DevicesController < Api::V1::ApplicationController
 
   def create
     @device = Device.where(device_params).first_or_create
-    if @device.person.nil?
-      person = Person.create
-      person.devices << @device
-    end
+    create_person_for @device if @device.person.nil?
     @current_user = @device.person
     @meta = { access_token: @device.access_token }
   end
@@ -14,4 +11,11 @@ private
   def device_params
     params.require(:device).permit(:identifier)
   end
+
+  def create_person_for device
+    person = Person.create
+    person.devices << device
+    person
+  end
+
 end
