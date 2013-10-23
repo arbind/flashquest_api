7b907488759cce8a151fa10d68e943eabc94a0c4
Arbind Thakur <arbind.thakur@gmail.com>
2013-10-22 22:48:44 -0700

    Load bootstrap data for admin screens

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,12 @@
 class Admin::AdminController < ApplicationController
   layout 'admin'
   http_basic_authenticate_with name: "flashquest", password: "flashqu3st"
+  before_action :bootstrap
+
+private 
+
+  def bootstrap
+    @flash_quest_biz = Business.where(name: FLASHQUEST_BIZ_NAME).first
+    @flash_quest_branch = @flash_quest_biz.branches.where(name: FLASHQUEST_BRANCH_NAME).first
+  end
 end
